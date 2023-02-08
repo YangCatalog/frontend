90ede3b66030ee141fbbfc5c07bb21c03990f380
dmytro-kyrychenko <dmytro.kyrychenko@pantheon.tech>
2023-02-08 17:58:05 +0200

    fix: changed checkIfRatified according to backend changes

--- yangcatalog-ui/src/app/features/yang-module-details/yang-module-details.component.ts
+++ yangcatalog-ui/src/app/features/yang-module-details/yang-module-details.component.ts
@@ -228,7 +228,7 @@ export class YangModuleDetailsComponent implements OnInit, OnDestroy {
   checkIfRatified(revision: string) {
     for (var revMat of this.infoData['revisions']) {
       if (revMat['revision'] == revision) {
-        return revMat['maturity-level'] == 'ratified';
+        return revMat['maturity_level'];
       }
     }
     return false;
